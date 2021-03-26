789ff85dfb2d48c04345e7b063eebffffdc7beed
Elijah Cobb <elijah@elijahcobb.com>
2021-03-26 16:35:53 -0400

    some client stuff figurted out

--- ts/index.ts
+++ ts/index.ts
@@ -37,7 +37,6 @@ interface Req {
 class DataStore {
 	private constructor() {}
 	public limit(bytes: number): DataStore { return this; }
-	public root(path: string): DataStore { return this; }
 	public auth(auth: Auth): DataStore { return this; }
 	public beforeSave(): DataStore { return this; }
 	public afterSave(): DataStore { return this; }
@@ -45,31 +44,37 @@ class DataStore {
 	public afterFetch(): DataStore { return this; }
 	public beforeDelete(): DataStore { return this; }
 	public afterDelete(): DataStore { return this; }
-	public static init(): DataStore { return new DataStore(); }
+	public static init(path: string): DataStore { return new DataStore(); }
 }
 
-class Copper {
+class CopperServer {
 
 	private constructor() {}
 
-	public server(port: number): Copper {
+	public server(port: number): CopperServer {
 		return this;
 	}
 
-	public database(host: string, port: number, database: string): Copper {
+	public database(host: string, port: number, database: string): CopperServer {
 		return this;
 	}
 
-	public defaultAuth(auth: Auth): Copper { return this; }
+	public enableAnonymousSession(): CopperServer { return this; }
 
-	public collection(collection: Collection): Copper {return this; }
+	public defaultAuth(auth: Auth): CopperServer { return this; }
 
-	public dataStore(dataStore: DataStore): Copper { return this; }
+	public collection(collection: Collection): CopperServer {return this; }
+
+	public cloudFunction(cloudFunction: CloudFunction<any, any>): CopperServer { return this; }
+
+	public dataStore(dataStore: DataStore): CopperServer { return this; }
 
 	public start(): void {}
 
-	public static init(): Copper {
-		return new Copper();
+	public groups(...groups: Group[]): CopperServer { return this; }
+
+	public static init(port: number): CopperServer {
+		return new CopperServer();
 	}
 }
 
@@ -82,10 +87,15 @@ class Auth {
 	public publicQuery(): Auth { return this; }
 	public publicFetch(): Auth { return this; }
 	public publicRead(): Auth { return this; }
-	public publicWrite(): Auth { return this; }
-	public publicCreate(): Auth { return this; }
-	public publicUpdate(): Auth { return this; }
-	public publicDelete(): Auth { return this; }
+
+	public anonymous(): Auth { return this; }
+	public anonymousQuery(): Auth { return this; }
+	public anonymousFetch(): Auth { return this; }
+	public anonymousRead(): Auth { return this; }
+	public anonymousWrite(): Auth { return this; }
+	public anonymousCreate(): Auth { return this; }
+	public anonymousUpdate(): Auth { return this; }
+	public anonymousDelete(): Auth { return this; }
 
 	public authenticated(): Auth { return this; }
 	public authenticatedQuery(): Auth { return this; }
@@ -96,14 +106,14 @@ class Auth {
 	public authenticatedUpdate(): Auth { return this; }
 	public authenticatedDelete(): Auth { return this; }
 
-	public admin(): Auth { return this; }
-	public adminQuery(): Auth { return this; }
-	public adminFetch(): Auth { return this; }
-	public adminRead(): Auth { return this; }
-	public adminWrite(): Auth { return this; }
-	public adminCreate(): Auth { return this; }
-	public adminUpdate(): Auth { return this; }
-	public adminDelete(): Auth { return this; }
+	public group(group: string): Auth { return this; }
+	public groupQuery(group: string): Auth { return this; }
+	public groupFetch(group: string): Auth { return this; }
+	public groupRead(group: string): Auth { return this; }
+	public groupWrite(group: string): Auth { return this; }
+	public groupCreate(group: string): Auth { return this; }
+	public groupUpdate(group: string): Auth { return this; }
+	public groupDelete(group: string): Auth { return this; }
 
 	public owner(): Auth { return this; }
 	public ownerQuery(): Auth { return this; }
@@ -123,15 +133,13 @@ class Auth {
 	public noUpdate(): Auth { return this; }
 	public noDelete(): Auth { return this; }
 
-	public reset(): Auth { return this; }
-
 	public static init(): Auth { return new Auth(); }
 }
 
 class Collection<T = any> {
 	private constructor() {}
 
-	public property<P>(name: string, type: OType<P>, required: boolean = false, defaultValue?: P): Collection<T> { return this; }
+	public property<P>(name: string, type: OType<P>, defaultValue?: P): Collection<T> { return this; }
 	public string(name: string, required: boolean = false, defaultValue: string = ""): Collection<T> { return this; }
 	public number(name: string, required: boolean = false, defaultValue: number = 0): Collection<T> { return this; }
 	public boolean(name: string, required: boolean = false, defaultValue: boolean = false): Collection<T> { return this; }
@@ -150,31 +158,56 @@ class Collection<T = any> {
 	public static init<T = any>(value: string): Collection<T> {return new Collection<T>(); }
 }
 
-Copper
-	.init()
-	.server(3000)
+class CloudFunction<P, R> {
+	private constructor() {}
+	public handler(handler: (param: P, meta: Meta) => Promise<R>): CloudFunction<P, R> { return this; }
+	public static init<P, R>(name: string, paramType: OType<P>, returnType: OType<R>): CloudFunction<P, R> { return new CloudFunction(); }
+}
+
+class Group {
+	private constructor() {}
+	public joinByDefault(): Group { return this; }
+	public static init(name: string): Group { return new Group(); }
+}
+
+CopperServer
+	.init(3000)
 	.database("localhost", 27017, "CopperTest")
+	.enableAnonymousSession()
 	.defaultAuth(
 		Auth
 			.init()
-			.admin()
+			.group("admin")
 	)
 	.dataStore(
 		DataStore
-			.init()
+			.init("/tmp")
 			.auth(
 				Auth
 					.init()
-					.admin()
+					.group("admin")
 			)
 			.limit(10_000_000)
-			.root("/tmp")
 			.beforeSave()
 			.afterSave()
 			.beforeFetch()
 			.afterFetch()
 			.beforeDelete()
 			.afterDelete()
+	)
+	.cloudFunction(
+		CloudFunction
+			.init("email", OStandardType.string, OStandardType.string)
+			.handler(async (p, m) => {
+				return "";
+			})
+	)
+	.groups(
+		Group
+			.init("admin"),
+		Group
+			.init("normal")
+			.joinByDefault()
 	)
 	.collection(
 		Collection
@@ -210,8 +243,8 @@ Copper
 			.auth(
 				Auth
 					.init()
-					.publicCreate()
-					.admin()
+					.authenticatedCreate()
+					.group("admin")
 					.owner()
 			)
 	)
@@ -220,7 +253,7 @@ Copper
 			.init<string>("Message")
 			.string("fromId", true)
 			.string("content", true)
-			.list("to", CuType.string)
+			.list("to", OStandardType.string)
 			.beforeCreate((doc, req) => {
 				console.log("Will be created!");
 			})
@@ -241,3 +274,98 @@ Copper
 			})
 	)
 	.start();
+
+
+export class Object {
+
+	public constructor(collection: string, owner: User) {
+
+	}
+
+	public get(): void {}
+	public set(): void {}
+	public setAuth(auth: Auth): void {}
+	public increment(key: string, value: number): void {}
+	public decrement(key: string, value: number): void {}
+	public addTo(key: string, value: number): void {}
+	public removeFrom(key: string, value: number): void {}
+
+	public save(): void {}
+	public delete(): void {}
+
+}
+
+class Copper {
+
+	private constructor() {
+	}
+
+	public auth(user: User): void {}
+
+	public object(collection: string): Object {
+		throw "";
+	}
+
+	public async signUp(email: string, password: string): Promise<User> { return new User(); }
+	public async signIn(email: string, password: string): Promise<User> { return new User(); }
+	public async token(token: string): Promise<User> { return new User(); }
+	public async anonymous(): Promise<AnonymousUser> { return new AnonymousUser(); }
+
+	public static async init(host: string, port: number): Promise<Copper> {
+		return new Copper();
+	}
+}
+
+class File {
+
+	public constructor(data: Buffer) {
+
+	}
+
+	public data(): Buffer { return Buffer.alloc(0); }
+	public getId(): string { return ""; }
+	public getOwnerId(): string { return ""; }
+	public getSize(): number { return 0; }
+
+	public async upload(): Promise<void> {}
+
+	public static async fetch(id: string): Promise<File> { return new File(Buffer.alloc(0)); }
+}
+
+export class User {
+
+	private salt: string = "";
+	private password: string = "";
+
+	public token(): string { return ""; }
+	public id(): string { return ""; }
+	public async delete(password: string): Promise<void> {}
+	public async logout(): Promise<void> {}
+	public async encrypt(data: string): Promise<string> { return ""; }
+	public async decrypt(data: string): Promise<string> { return ""; }
+
+	public static async signUp(email: string, password: string): Promise<User> { return new User(); }
+	public static async signIn(email: string, password: string): Promise<User> { return new User(); }
+	public static async token(token: string): Promise<User> { return new User(); }
+}
+
+export class AnonymousUser extends User {
+
+	public constructor() {
+		super();
+	}
+
+	public async delete(): Promise<void> {
+		await super.delete("");
+	}
+
+	public static async anonymous(): Promise<AnonymousUser> { return new AnonymousUser(); }
+
+}
+
+const api = await Copper.init("localhost", 3000);
+const u = await api.signIn("efwewf", "wef");
+
+
+const book = api.object("Book");
+book.set();
